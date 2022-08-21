8ad4e10765ef504a449a68b8272e98b2aa86beaa
Alisson Peixer <alissonpeixer4@gmail.com>
2022-08-21 16:09:59 -0300

    feat: Frist commit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,13 +2,16 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export default NextAuth({
-  // Configure one or more authentication providers
+  theme: {
+    logo: '/logo.svg'
+  },
   providers: [
     GithubProvider({
       clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID || "",
       clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET_KEY || "",
     }),
     // ...add more providers here
+    
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
