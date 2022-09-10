22d53b9ea8849e86408067083993fa74ed8f36e6
Alisson Pexer <alissonpeixer4@gmail.com>
2022-09-10 14:44:17 -0300

    feat: added logo input and shadow in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
-import Link from 'next/link';
-import { ButtonAuthenticate } from './ButtonAuthenticate';
-import { Avatar } from './Avatar';
 
+import { ButtonAuthenticate } from "./ButtonAuthenticate";
+import { Avatar } from "./Avatar";
+import { LogoInput } from "./Logo";
 export const Header = () => {
 	return (
-		<header className='w-full'>
-			<div className='container flex items-center  max-w-7xl h-24 mx-auto'>
-				<div className='flex-1'>
-					<Link href={'/'}>Show Me</Link>
+		<header className="w-full bg-bastille-800 shadow-xl">
+			<div className="container flex items-center  max-w-7xl h-24 mx-auto p-4">
+				<div className="flex-1">
+					<LogoInput  width={150} />
 				</div>
 
-				<div className='flex w-20 justify-between'>
+				<div className="flex w-20 justify-between">
 					<Avatar width={40} height={40} />
 					<ButtonAuthenticate />
 				</div>
