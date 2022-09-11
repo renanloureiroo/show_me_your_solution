56d392910311dd1cf72b1df18c39bba2a98797eb
Alisson Pexer <alissonpeixer4@gmail.com>
2022-09-11 20:31:49 -0300

    refactor(file): created components(session)

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -1,33 +1,30 @@
 import * as React from "react";
 import { useRouter } from "next/router";
-import { useSession, signOut, signIn } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import MenuItem from "@mui/material/MenuItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import Divider from "@mui/material/Divider";
 
-import { Books, AddressBook, SignIn, CircleNotch, SignOut } from "phosphor-react";
+import { Books, AddressBook,  CircleNotch, UserCircle  } from "phosphor-react";
+
+import { Login } from "./Login";
+import { Logout } from "./Logout";
 
 export const MenuItems = () => {
 	const router = useRouter();
 	const { data: session, status } = useSession();
 
-	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
-	const open = Boolean(anchorEl);
-
-	const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorEl(event.currentTarget);
-	};
-
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
 
 	return (
 		<>
 			{status === "authenticated" ? (
 				<>
-					<MenuItem>Minha conta</MenuItem>
+					<MenuItem onClick={() => router.push("/user")}>
+						<ListItemIcon>
+							<UserCircle size={22} />
+						</ListItemIcon>
+						Minha conta
+					</MenuItem>
 					
 					<Divider />
 
@@ -44,17 +41,13 @@ export const MenuItems = () => {
 						Desafios
 					</MenuItem>
 
-					<MenuItem onClick={() => signOut()}>
-						<ListItemIcon>
-							<SignOut size={22} />
-						</ListItemIcon>
-						Sair
+					<MenuItem >
+						<Logout/>
 					</MenuItem>
 				</>
 			) : status === "unauthenticated" ? (
-				<MenuItem onClick={() => signIn()}>
-					<SignIn className="mr-2" size={22} />
-					Login
+				<MenuItem >
+					<Login/>
 				</MenuItem>
 			) : (
 				<CircleNotch size={32} className="animate-spin" />
@@ -62,3 +55,4 @@ export const MenuItems = () => {
 		</>
 	);
 };
+
