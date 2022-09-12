d5dad94d35e692ed2437014f7781f6c09edd5be5
Alisson Pexer <alissonpeixer4@gmail.com>
2022-09-11 21:53:42 -0300

    feat: added hreft in logo header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,15 @@
-
-
+import { useRouter } from "next/router";
 import { LogoInput } from "./Logo";
 import { MenuUser } from "./MenuUser";
 
 export const Header = () => {
+	const router = useRouter()
 	return (
 		<header className="w-full transition-all absolute ">
 			<div className="flex items-center h-24 p-4 container mx-auto">
-				<div className="flex-1">
+				<a className="flex-1 cursor-pointer" onClick={ () =>router.push('/')} >
 					<LogoInput  width={150} />
-				</div>
+				</a>
 
 				<div className="flex space-x-2">
 					<MenuUser />
